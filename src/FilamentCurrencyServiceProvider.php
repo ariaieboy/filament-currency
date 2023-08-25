5b49573c33d4aea24f0669dd8e626159c9ce5752
ariaieboy <ariaieboy.ir@gmail.com>
2023-08-25 18:54:54 +0330

    add currency macro to TextColumn

--- src/FilamentCurrencyServiceProvider.php
+++ src/FilamentCurrencyServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Ariaieboy\FilamentCurrency;
 
+use Akaunting\Money;
+use Closure;
+use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\TextColumn;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -14,5 +18,28 @@ class FilamentCurrencyServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name);
+    }
+
+    public function bootingPackage(): void
+    {
+        TextColumn::macro('currency', function (string|Closure|null $currency = null, bool $shouldConvert = false): static {
+            $this->formatStateUsing(static function (Column $column, $state) use ($currency, $shouldConvert): ?string {
+                if (blank($state)) {
+                    return null;
+                }
+
+                if (blank($currency)) {
+                    $currency = env('DEFAULT_CURRENCY', 'USD');
+                }
+
+                return (new Money\Money(
+                    $state,
+                    (new Money\Currency(strtoupper($column->evaluate($currency)))),
+                    $shouldConvert,
+                ))->format();
+            });
+
+            return $this;
+        });
     }
 }
