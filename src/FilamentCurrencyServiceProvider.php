8e3cca6e5cbe4037ccffb696c93a492d5fa03595
ariaieboy <ariaieboy@users.noreply.github.com>
2023-08-25 15:20:31 +0000

    Fix styling

--- src/FilamentCurrencyServiceProvider.php
+++ src/FilamentCurrencyServiceProvider.php
@@ -2,18 +2,6 @@
 
 namespace Ariaieboy\FilamentCurrency;
 
-use Ariaieboy\FilamentCurrency\Commands\FilamentCurrencyCommand;
-use Ariaieboy\FilamentCurrency\Testing\TestsFilamentCurrency;
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Filament\Support\Icons\Icon;
-use Illuminate\Filesystem\Filesystem;
-use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
